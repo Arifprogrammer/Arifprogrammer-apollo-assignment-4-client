a70472e61c6ad3a7ab0eabbf7694f4d362603ac5
Arifprogrammer <arifmgtcu@gmail.com>
2024-07-10 18:41:17 +0600

    showing navbar

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../shared/Navbar";
 
 const MainLayout = () => {
   return (
     <>
+      <Navbar />
       <div className="min-h-[80vh] pt-16">
         <Outlet />
       </div>
---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,120 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const listItem = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "text-red-700 font-semibold lg:border-b-4 border-b-red-700"
+              : "font-semibold lg:border-b-4 lg:border-b-white hover:border-b-red-700 lg:text-white hover:text-red-700 lg:transition lg:duration-200"
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/products"
+          className={({ isActive }) =>
+            isActive
+              ? "text-red-700 font-semibold lg:border-b-4 border-b-red-700"
+              : "font-semibold lg:border-b-4 lg:border-b-white hover:border-b-red-700 lg:text-white hover:text-red-700 lg:transition lg:duration-200"
+          }
+        >
+          Products
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive
+              ? "text-red-700 font-semibold lg:border-b-4 border-b-red-700"
+              : "font-semibold lg:border-b-4 lg:border-b-white hover:border-b-red-700 lg:text-white hover:text-red-700 lg:transition lg:duration-200"
+          }
+        >
+          About Us
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive
+              ? "text-red-700 font-semibold lg:border-b-4 border-b-red-700"
+              : "font-semibold lg:border-b-4 lg:border-b-white hover:border-b-red-700 lg:text-white hover:text-red-700 lg:transition lg:duration-200"
+          }
+        >
+          Contact Us
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            isActive
+              ? "text-red-700 font-semibold lg:border-b-4 border-b-red-700"
+              : "font-semibold lg:border-b-4 lg:border-b-white hover:border-b-red-700 lg:text-white hover:text-red-700 lg:transition lg:duration-200"
+          }
+        >
+          Dashboard
+        </NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <section className="fixed z-10 w-full bg-black">
+      <div className="navbar p-0 my-container rounded-sm text-white">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-100 rounded-box w-52 font-semibold text-black"
+            >
+              {listItem}
+            </ul>
+          </div>
+          <div className="hidden lg:flex">
+            <h1 className="text-2xl font-semibold">
+              The Key<span className="italic text-red-700">stroke</span>
+            </h1>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <div className="lg:hidden">
+            <h1 className="text-xl font-semibold">
+              The Key<span className="italic text-red-700">stroke</span>
+            </h1>
+          </div>
+        </div>
+        <div className="navbar-end">
+          <ul className="menu menu-horizontal mr-3 gap-4 hidden lg:flex">
+            {listItem}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
