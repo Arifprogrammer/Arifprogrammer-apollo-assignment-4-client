3ed693d855dd81f9c1a0dffdc1e2d3208d3d0b7e
Arifprogrammer <arifmgtcu@gmail.com>
2024-07-10 18:36:13 +0600

    first commit

---
+++ src/routes/router.tsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/home/Home";
+import Products from "../pages/products/Products";
+import About from "../pages/about/About";
+import Dashboard from "../pages/dashboard/Dashboard";
+import Contact from "../pages/contact/Contact";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
